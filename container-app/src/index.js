d69f746b325723c329748275951a0456bf739d15
Shivam Prajapati <shivam@thegeeksquad.tech>
2021-12-23 22:26:46 +0530

    commiting

--- container-app/src/index.js
+++ container-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// You can write your own logic here to determine the actual url
+window.app2Url = 'http://localhost:8002';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
--- cra-host/src/App.js
+++ cra-host/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+	const [input, setInput] = useState('');
+	const { message, updateScore } = props;
+	let { view = 'default-view' } = props;
+	return (
+		<div
+			style={{
+				margin: '10px',
+				padding: '10px',
+				textAlign: 'center',
+				backgroundColor: 'cyan',
+			}}>
+			<h1>App 2</h1>
+			<hr></hr>
+			<p>Message from APp1</p>
+			<p>{message}</p>
+			{view === 'table-view' ? (
+				<div>
+					{' '}
+					<p>Table view</p>
+					<input
+						type='number'
+						value={input}
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}></input>
+					<button
+						onClick={() => {
+							updateScore(input);
+						}}>
+						Update Score
+					</button>
+				</div>
+			) : (
+				<p>You are viewing the default view</p>
+			)}
+		</div>
+	);
+};
 
 export default App;
